7e373b292db63458ec2c5d235e3e4274f6aafde7
redking00@hotmail.com <redking00@hotmail.com>
2024-11-14 17:53:04 +0100

    Initial commit

---
+++ example.nb.ts
@@ -0,0 +1,14 @@
+
+//#nbts@mark
+// # This is a DenoNBTS lsp simple test
+//#nbts@code
+type Person = {
+	name: string
+	age: number
+}
+//#nbts@code
+const me: Person = {
+	name: 'Bob'
+}
+
+me.age = 30;
